e5f58ec8ba8eb05f3a6d59335dfddf59798a3c67
Kyle Gerard Felker <felker@anl.gov>
2021-08-20 00:37:57 +0000

    Fix typos

--- hdf5/1.8.13.lua
+++ hdf5/1.8.13.lua
@@ -2,8 +2,6 @@ help([[
 Parallel HDF5 library version 1.8.13, built with CUDA-aware OpenMPI
 ]])
 
-depends_on("openmpi/openmpi-4.0.5")
-
 whatis("Name: HDF5")
 whatis("Version: 1.8.13")
 whatis("Category: hdf5")
@@ -11,12 +9,14 @@ whatis("Keywords: hdf5")
 whatis("Description: Parallel HDF5 library")
 whatis("URL: https://www.hdfgroup.org/solutions/hdf5/")
 
+depends_on("openmpi/openmpi-4.0.5")
+
 local hdf5_root = "/lus/theta-fs0/software/thetagpu/hdf5/1.8.13/"
 setenv("HDF5_ROOT",hdf5_root)
 setenv("HDF5_DIR",hdf5_root)
 
-prepend-path("PATH",pathJoin(hdf5_root,"bin/")
-prepend-path("CPATH",pathJoin(hdf5_root,"include/")
-prepend-path("LD_LIBRARY_PATH",pathJoin(hdf5_root,"lib/")
+prepend_path("PATH",pathJoin(hdf5_root,"bin/"))
+prepend_path("CPATH",pathJoin(hdf5_root,"include/"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(hdf5_root,"lib/"))
 
 family("hdf5")
