27af1cb0e4a33c0a0820eb221764c41e709d8d64
Kyle Gerard Felker <felker@anl.gov>
2021-08-20 01:19:02 +0000

    Add simple hypre install script

---
+++ install_hypre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /lus/theta-fs0/software/thetagpu/
+git clone https://github.com/hypre-space/hypre.git
+cd hypre/src
+
+# git checkout v2.22.0
+make clean
+
+# https://github.com/hypre-space/hypre
+./configure --with-cuda --with-gpu-arch='80' --with-cuda-home=/usr/local/cuda-11.3 --enable-gpu-profiling --enable-cusparse --enable-cublas --enable-curand --enable-device-memory-pool --enable-unified-memory --enable-shared --enable-gpu-aware-mpi
+
+make -j128
+
+# https://hypre.readthedocs.io/en/latest/ch-misc.html#linking-to-the-library
