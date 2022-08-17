e0b11fffed9afdf051b34181518c004883fa7e5d
Kyle Gerard Felker <felker@anl.gov>
2022-08-17 18:36:38 +0000

    Replacing Huihuo's install of 1.13.1

--- modulefiles/hdf5/1.13.1-nvhpc.lua
+++ modulefiles/hdf5/1.13.1-nvhpc.lua
@@ -1,10 +1,10 @@
 help([[
-Parallel HDF5 library version 1.12.2, built with OpenMPI (wrapping NVIDIA HPC compilers)
+Parallel HDF5 library version 1.13.1, built with OpenMPI (wrapping NVIDIA HPC compilers)
 Built with Fortran support
 ]])
 
 whatis("Name: HDF5")
-whatis("Version: 1.12.2")
+whatis("Version: 1.13.1")
 whatis("Category: hdf5")
 whatis("Keywords: hdf5")
 whatis("Description: Parallel HDF5 library")
@@ -13,7 +13,7 @@ whatis("URL: https://www.hdfgroup.org/solutions/hdf5/")
 depends_on("openmpi/openmpi-4.1.4_ucx-1.12.1_nvhpc-21.7")
 depends_on("nvhpc-nompi/21.7")
 
-local hdf5_root = "/lus/theta-fs0/software/thetagpu/hdf5/1.12.2-nvhpc/"
+local hdf5_root = "/lus/theta-fs0/software/thetagpu/hdf5/1.13.1-nvhpc/"
 setenv("HDF5_ROOT",hdf5_root)
 setenv("HDF5_DIR",hdf5_root)
 -- setenv("UCX_LOG_LEVEL","error")
